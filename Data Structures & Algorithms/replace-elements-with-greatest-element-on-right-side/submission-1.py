6df41f93ca5c3e4c6a1f96c2660dcb8733922002
Prithbiraj Dhar <pdharstudies@gmail.com>
2026-05-30 16:03:35 +0530

    Refactor replaceElements method in submission-1.py
    Removed commented code and added logic to replace elements.

--- Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.py
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.py
@@ -1,10 +1,5 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        #initial max=-1
-        #rev iterate
-        #new max= max(oldmax, arr[i])
-
-        
         rightMax = -1
         for i in range(len(arr)-1,-1, -1):
             old = arr[i]
